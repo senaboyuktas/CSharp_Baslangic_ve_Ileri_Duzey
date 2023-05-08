06b90286fa7ecfa4e0cb88d0942db17ba37139be
senaboyuktas <senabykts99@gmail.com>
2023-05-08 17:17:44 +0300

    113. ders sonu

---
+++ Metot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; // Bir kutuphane dosyasını uygulamamızın içerisine eklemektir.
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uzun bir şekilde yazım. 
+            //System.Console.Write("Merhaba");
+            // using kısmında kütüphane eklenmiş hali ile yazımı.
+            //Console.Write("Merhaba");
+            Selamla();
+
+            Ogrenci O1 = new Ogrenci();
+            O1.OgrenciMetot2();
+            //O1.selamlaOgrenci();
+        }
+        static void Selamla()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
---
+++ MetotOdev/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogrenciAd = string.Empty;
+            string ogrenciSoyad = string.Empty;
+            decimal not1 = 0;
+            decimal not2 = 0;
+            decimal not3 = 0;
+            decimal ortalama = 0;
+
+            Console.WriteLine("Merhaba Ortalamasını hesaplamak istediğiniz öğrencinin bilgilerini giriniz");
+
+            Console.Write("Ad :");
+            ogrenciAd = Console.ReadLine();
+
+            Console.Write("Soyad :");
+            ogrenciSoyad = Console.ReadLine();
+
+            Console.Write("Not 1:");
+            not1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Not 2:");
+            not2 = decimal.Parse(Console.ReadLine());
+
+            //Console.Write("Not 3:");
+            //not3 = decimal.Parse(Console.ReadLine());
+
+            Ogrenci O = new Ogrenci();
+            O.ogrenciNotHesapla(ogrenciAd, ogrenciSoyad, not1, not2);
+        }
+    }
+}
