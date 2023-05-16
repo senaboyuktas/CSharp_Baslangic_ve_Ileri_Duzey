278fe741bb25dc851ffc47f4f6c97e0b446c9987
senaboyuktas <senabykts99@gmail.com>
2023-05-16 15:12:40 +0300

    199. ders sonu

---
+++ GenericClassNedir_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassNedir_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+             * 
+             * Database  | Uygulama
+             * MSSQL       C#
+             * 
+             * tblMusteri
+             * id int
+             * musteriNumara uniqueidentitfy ( Guid ) 
+             * isim nvarchar(50) string 
+             * soyisim nvarchar(50) string 
+             * dogumTarih datetime  datetime
+             * 
+             * UnitOfWorks => Generic Class |Generic Interface 
+             * 
+             */
+
+
+
+			genericRepository<Musteri> repositoryMusteri = new genericRepository<Musteri>();
+
+
+			List<Musteri> musterilerim = repositoryMusteri.Getir();
+
+			repositoryMusteri.yeniKayitEkle(null);
+
+			genericRepository<Urun> repositoryUrun = new genericRepository<Urun>();
+			repositoryUrun.Getir();
+			repositoryUrun.yeniKayitEkle(null);
+		}
+	}
+}
---
+++ KullaniciGirisUygulama/anaForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace KullaniciGirisUygulama
+{
+	partial class anaForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblSelamla = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtIsim = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtSoyisim = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txtSifre = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.txtAciklama = new System.Windows.Forms.TextBox();
+			this.btnGuncelle = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblSelamla
+			// 
+			this.lblSelamla.AutoSize = true;
+			this.lblSelamla.Location = new System.Drawing.Point(78, 39);
+			this.lblSelamla.Name = "lblSelamla";
+			this.lblSelamla.Size = new System.Drawing.Size(0, 16);
+			this.lblSelamla.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(37, 16);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "İsim :";
+			// 
+			// txtIsim
+			// 
+			this.txtIsim.Location = new System.Drawing.Point(96, 22);
+			this.txtIsim.Name = "txtIsim";
+			this.txtIsim.Size = new System.Drawing.Size(100, 22);
+			this.txtIsim.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(78, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 16);
+			this.label2.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(29, 65);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(61, 16);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Soyisim :";
+			// 
+			// txtSoyisim
+			// 
+			this.txtSoyisim.Location = new System.Drawing.Point(96, 59);
+			this.txtSoyisim.Name = "txtSoyisim";
+			this.txtSoyisim.Size = new System.Drawing.Size(100, 22);
+			this.txtSoyisim.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(301, 42);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(0, 16);
+			this.label4.TabIndex = 0;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(252, 28);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(85, 16);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "Kullanıcı Adı :";
+			// 
+			// txtKullaniciAdi
+			// 
+			this.txtKullaniciAdi.Location = new System.Drawing.Point(343, 25);
+			this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+			this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 22);
+			this.txtKullaniciAdi.TabIndex = 2;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(301, 81);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(0, 16);
+			this.label6.TabIndex = 0;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(252, 67);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(40, 16);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "Şifre :";
+			// 
+			// txtSifre
+			// 
+			this.txtSifre.Location = new System.Drawing.Point(343, 61);
+			this.txtSifre.Name = "txtSifre";
+			this.txtSifre.Size = new System.Drawing.Size(100, 22);
+			this.txtSifre.TabIndex = 2;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(78, 116);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(0, 16);
+			this.label8.TabIndex = 0;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(29, 102);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(69, 16);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Açıklama :";
+			// 
+			// txtAciklama
+			// 
+			this.txtAciklama.Location = new System.Drawing.Point(32, 121);
+			this.txtAciklama.Multiline = true;
+			this.txtAciklama.Name = "txtAciklama";
+			this.txtAciklama.Size = new System.Drawing.Size(411, 117);
+			this.txtAciklama.TabIndex = 2;
+			// 
+			// btnGuncelle
+			// 
+			this.btnGuncelle.Location = new System.Drawing.Point(32, 244);
+			this.btnGuncelle.Name = "btnGuncelle";
+			this.btnGuncelle.Size = new System.Drawing.Size(411, 39);
+			this.btnGuncelle.TabIndex = 3;
+			this.btnGuncelle.Text = "Güncelle";
+			this.btnGuncelle.UseVisualStyleBackColor = true;
+			this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+			// 
+			// anaForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(474, 293);
+			this.Controls.Add(this.btnGuncelle);
+			this.Controls.Add(this.txtSifre);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.txtKullaniciAdi);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtAciklama);
+			this.Controls.Add(this.txtSoyisim);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.txtIsim);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.lblSelamla);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "anaForm";
+			this.Text = "Aktif Kullanıcı Bilgisi";
+			this.Load += new System.EventHandler(this.anaForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblSelamla;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtIsim;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtSoyisim;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtKullaniciAdi;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox txtSifre;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox txtAciklama;
+		private System.Windows.Forms.Button btnGuncelle;
+	}
+}
